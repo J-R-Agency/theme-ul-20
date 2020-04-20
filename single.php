e859822df017edf9786b4a47cb5de0bfae8d7ffd
Joy Ling <joy@joys-mbp.home>
2020-04-20 17:04:51 +0100

    Blog post changes

--- single.php
+++ single.php
@@ -34,7 +34,41 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 			<?php get_template_part( 'loop-templates/content', 'single' ); ?>
 
-			<?php understrap_post_nav(); ?>
+			<div class="post-navigation">
+				<div class="nav-links">
+					<?php
+						$post_id = $post->ID; // current post ID
+									
+						$args = array( 
+						    'orderby'  => 'post_date',
+						    'order'    => 'DESC'
+						);
+						$posts = get_posts( $args );
+						// get IDs of posts retrieved from get_posts
+						$ids = array();
+						foreach ( $posts as $thepost ) {
+						    $ids[] = $thepost->ID;
+						}
+						// get and echo previous and next post in the same category
+						$thisindex = array_search( $post_id, $ids );
+						$previd    = isset( $ids[ $thisindex - 1 ] ) ? $ids[ $thisindex - 1 ] : 0;
+						$allid     = isset( $ids[ $thisindex] ) ? $ids[ $thisindex] : 0;
+						$nextid    = isset( $ids[ $thisindex + 1 ] ) ? $ids[ $thisindex + 1 ] : 0;
+						
+						if ( $previd ) {
+						    ?><span><a id="prev" rel="prev" href="<?php echo get_permalink($previd) ?>"><p> < Previous post</p></a></span>
+						    <?php
+						}
+						if ( $allid ) {
+							?><span><a id="all" href="<?php echo site_url();?>/news" class="link"><div class="btn_red-border">View All</div></a></span><?php
+						}
+						
+						if ( $nextid ) {
+						    ?><span><a id="next" rel="next" href="<?php echo get_permalink($nextid) ?>"><p>Next post ></p></a></span><?php
+						}
+					?>
+				</div>
+			</div>
 
 		<?php endwhile; // end of the loop. ?>
 	</div>
