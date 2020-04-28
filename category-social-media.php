a9c65e6e7c5eab542113900cc8f842c740b77b1f
Joy Ling <joy@joys-mbp.home>
2020-04-28 10:14:48 +0100

    Renamed archive templates

--- category-social-media.php
+++ category-social-media.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Blog Archive Template
+ * Template Name: Social Media Archive Template
  *
  * Template for displaying a page just with the header and footer area and a "naked" content area in between.
  * Good for landingpages and other types of pages where you want to add a lot of custom markup.
