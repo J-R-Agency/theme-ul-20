93038506f1b59a8529766f5c60c9787ad90e43ce
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-09-17 09:21:39 +0100

    Mailchimp integration code in header

--- header.php
+++ header.php
@@ -30,6 +30,9 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
+
+	<script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/993d55a720edfca88b9c43984/0fcbe1472d196983d18fa953d.js");</script>
+
 </head>
 
 <body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
